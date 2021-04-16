a1555013bae67905d91210a6528559069f494d65
Kim, Sung eun <sungeun.kim@zf.com>
2021-04-16 16:59:55 +0900

    About text reveal.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
 
 const StyledAbout = styled.div`
   display: flex;
@@ -39,34 +40,40 @@ const StyledBio = styled.div`
 `
 
 function About() {
-    return (
-        <StyledAbout>
-          <StyledPhoto><img src='/assets/portrait.jpg'></img></StyledPhoto>
-          <StyledBio>
-            <p>
-              SeogGyeong(March 23, 1952 ~) is contemporary Korean painting artist. 
-            </p>
-            <p>
-              She was born in Andong, a small city placed in southern east area of South Korea, March 1952.
-              She was raised in a devout Christian family.
-              Since childhood, she was attracted by living in the nature, and she actually tried it when she was thirteen.
-            </p>
-            <p>
-              She graduated Kyungpook National University as a chemistry teacher. 
-              And married with present husband and moved to Belfort, France following him that assigned as expatriate.
-              At that time she had many opportunities to travel various European countries and there she influenced by paintings, arts and culture there.
-            </p>
-            <p>
-              She is beloved wife, mother of two son and grandmother of four kids.
-              She is painting through her whole life time.
-              And Still dreaming the life in nature with the believe in god's plan.
-            </p>
-            <p>
-              2021 is the year of her 70th birth anniversary.
-            </p>
-          </StyledBio>
-        </StyledAbout>
-    )
+  return (
+    <StyledAbout>
+      <StyledPhoto>
+      <Fade bottom>
+        <img src='/assets/portrait.jpg' alt='Portrait of SeogGyeong'></img>
+        </Fade>
+        </StyledPhoto>
+      <StyledBio>
+      <Fade bottom>
+        <p>
+          SeogGyeong(March 23, 1952 ~) is contemporary Korean painting artist. 
+        </p>
+        <p>
+          She was born in Andong, a small city placed in southern east area of South Korea, March 1952.
+          She was raised in a devout Christian family.
+          Since childhood, she was attracted by living in the nature, and she actually tried it when she was thirteen.
+        </p>
+        <p>
+          She graduated Kyungpook National University as a chemistry teacher. 
+          And married with present husband and moved to Belfort, France following him that assigned as expatriate.
+          At that time she had many opportunities to travel various European countries and there she influenced by paintings, arts and culture there.
+        </p>
+        <p>
+          She is beloved wife, mother of two son and grandmother of four kids.
+          She is painting through her whole life time.
+          And Still dreaming the life in nature with the believe in god's plan.
+        </p>
+        <p>
+          2021 is the year of her 70th birth anniversary.
+        </p>
+      </Fade>
+      </StyledBio>
+    </StyledAbout>
+  )
 }
 
 export default About
