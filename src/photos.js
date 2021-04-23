2f38272f974f994402317ccf50aa2e14acf9a9b1
Kim, Sung eun <su79eu7k@gmail.com>
2021-04-23 23:26:09 +0900

    decode error resolved.

--- src/photos.js
+++ src/photos.js
@@ -1,206 +1,206 @@
 export const photos = [
   {
-    original: "../../assets/original/20150411_112558.jpg",
+    original: "../../assets/optimized/20150411_112558.jpg",
     thumbnail: "../../assets/thumbnail/20150411_112558.jpg",
   },
   {
-    original: "../../assets/original/20170827_145355.jpg",
+    original: "../../assets/optimized/20170827_145355.jpg",
     thumbnail: "../../assets/thumbnail/20170827_145355.jpg",
   },
   {
-    original: "../../assets/original/20171122_221041.jpg",
+    original: "../../assets/optimized/20171122_221041.jpg",
     thumbnail: "../../assets/thumbnail/20171122_221041.jpg",
   },
   {
-    original: "../../assets/original/20180102_184812.jpg",
+    original: "../../assets/optimized/20180102_184812.jpg",
     thumbnail: "../../assets/thumbnail/20180102_184812.jpg",
   },
   {
-    original: "../../assets/original/20180514_215817.jpg",
+    original: "../../assets/optimized/20180514_215817.jpg",
     thumbnail: "../../assets/thumbnail/20180514_215817.jpg",
   },
   {
-    original: "../../assets/original/20180608_112111.jpg",
+    original: "../../assets/optimized/20180608_112111.jpg",
     thumbnail: "../../assets/thumbnail/20180608_112111.jpg",
   },
   {
-    original: "../../assets/original/20180706_165409.jpg",
+    original: "../../assets/optimized/20180706_165409.jpg",
     thumbnail: "../../assets/thumbnail/20180706_165409.jpg",
   },
   {
-    original: "../../assets/original/20180725_204018.jpg",
+    original: "../../assets/optimized/20180725_204018.jpg",
     thumbnail: "../../assets/thumbnail/20180725_204018.jpg",
   },
   {
-    original: "../../assets/original/20180823_111031.jpg",
+    original: "../../assets/optimized/20180823_111031.jpg",
     thumbnail: "../../assets/thumbnail/20180823_111031.jpg",
   },
   {
-    original: "../../assets/original/20180824_165756.jpg",
+    original: "../../assets/optimized/20180824_165756.jpg",
     thumbnail: "../../assets/thumbnail/20180824_165756.jpg",
   },
   {
-    original: "../../assets/original/20180914_204950.jpg",
+    original: "../../assets/optimized/20180914_204950.jpg",
     thumbnail: "../../assets/thumbnail/20180914_204950.jpg",
   },
   {
-    original: "../../assets/original/20180915_114321.jpg",
+    original: "../../assets/optimized/20180915_114321.jpg",
     thumbnail: "../../assets/thumbnail/20180915_114321.jpg",
   },
   {
-    original: "../../assets/original/20180924_202144.jpg",
+    original: "../../assets/optimized/20180924_202144.jpg",
     thumbnail: "../../assets/thumbnail/20180924_202144.jpg",
   },
   {
-    original: "../../assets/original/20181018_170954.jpg",
+    original: "../../assets/optimized/20181018_170954.jpg",
     thumbnail: "../../assets/thumbnail/20181018_170954.jpg",
   },
   {
-    original: "../../assets/original/20181109_171629.jpg",
+    original: "../../assets/optimized/20181109_171629.jpg",
     thumbnail: "../../assets/thumbnail/20181109_171629.jpg",
   },
   {
-    original: "../../assets/original/20181203_223148.jpg",
+    original: "../../assets/optimized/20181203_223148.jpg",
     thumbnail: "../../assets/thumbnail/20181203_223148.jpg",
   },
   {
-    original: "../../assets/original/20190115_103449.jpg",
+    original: "../../assets/optimized/20190115_103449.jpg",
     thumbnail: "../../assets/thumbnail/20190115_103449.jpg",
   },
   {
-    original: "../../assets/original/20190204_202748.jpg",
+    original: "../../assets/optimized/20190204_202748.jpg",
     thumbnail: "../../assets/thumbnail/20190204_202748.jpg",
   },
   {
-    original: "../../assets/original/20190303_221742.jpg",
+    original: "../../assets/optimized/20190303_221742.jpg",
     thumbnail: "../../assets/thumbnail/20190303_221742.jpg",
   },
   {
-    original: "../../assets/original/20190304_111058.jpg",
+    original: "../../assets/optimized/20190304_111058.jpg",
     thumbnail: "../../assets/thumbnail/20190304_111058.jpg",
   },
   {
-    original: "../../assets/original/20190319_200032.jpg",
+    original: "../../assets/optimized/20190319_200032.jpg",
     thumbnail: "../../assets/thumbnail/20190319_200032.jpg",
   },
   {
-    original: "../../assets/original/20190420_204712.jpg",
+    original: "../../assets/optimized/20190420_204712.jpg",
     thumbnail: "../../assets/thumbnail/20190420_204712.jpg",
   },
   {
-    original: "../../assets/original/20190517_174226.jpg",
+    original: "../../assets/optimized/20190517_174226.jpg",
     thumbnail: "../../assets/thumbnail/20190517_174226.jpg",
   },
   {
-    original: "../../assets/original/20190526_134138.jpg",
+    original: "../../assets/optimized/20190526_134138.jpg",
     thumbnail: "../../assets/thumbnail/20190526_134138.jpg",
   },
   {
-    original: "../../assets/original/20190531_201904.jpg",
+    original: "../../assets/optimized/20190531_201904.jpg",
     thumbnail: "../../assets/thumbnail/20190531_201904.jpg",
   },
   {
-    original: "../../assets/original/20190711_213931.jpg",
+    original: "../../assets/optimized/20190711_213931.jpg",
     thumbnail: "../../assets/thumbnail/20190711_213931.jpg",
   },
   {
-    original: "../../assets/original/20190816_215111.jpg",
+    original: "../../assets/optimized/20190816_215111.jpg",
     thumbnail: "../../assets/thumbnail/20190816_215111.jpg",
   },
   {
-    original: "../../assets/original/20190817_181256.jpg",
+    original: "../../assets/optimized/20190817_181256.jpg",
     thumbnail: "../../assets/thumbnail/20190817_181256.jpg",
   },
   {
-    original: "../../assets/original/20190819_113908.jpg",
+    original: "../../assets/optimized/20190819_113908.jpg",
     thumbnail: "../../assets/thumbnail/20190819_113908.jpg",
   },
   {
-    original: "../../assets/original/20190901_071546.jpg",
+    original: "../../assets/optimized/20190901_071546.jpg",
     thumbnail: "../../assets/thumbnail/20190901_071546.jpg",
   },
   {
-    original: "../../assets/original/20191015_132749.jpg",
+    original: "../../assets/optimized/20191015_132749.jpg",
     thumbnail: "../../assets/thumbnail/20191015_132749.jpg",
   },
   {
-    original: "../../assets/original/20191118_200820.jpg",
+    original: "../../assets/optimized/20191118_200820.jpg",
     thumbnail: "../../assets/thumbnail/20191118_200820.jpg",
   },
   {
-    original: "../../assets/original/20191118_201126.jpg",
+    original: "../../assets/optimized/20191118_201126.jpg",
     thumbnail: "../../assets/thumbnail/20191118_201126.jpg",
   },
   {
-    original: "../../assets/original/20191120_221517.jpg",
+    original: "../../assets/optimized/20191120_221517.jpg",
     thumbnail: "../../assets/thumbnail/20191120_221517.jpg",
   },
   {
-    original: "../../assets/original/20191120_222116.jpg",
+    original: "../../assets/optimized/20191120_222116.jpg",
     thumbnail: "../../assets/thumbnail/20191120_222116.jpg",
   },
   {
-    original: "../../assets/original/20191120_222137.jpg",
+    original: "../../assets/optimized/20191120_222137.jpg",
     thumbnail: "../../assets/thumbnail/20191120_222137.jpg",
   },
   {
-    original: "../../assets/original/20191120_222448.jpg",
+    original: "../../assets/optimized/20191120_222448.jpg",
     thumbnail: "../../assets/thumbnail/20191120_222448.jpg",
   },
   {
-    original: "../../assets/original/20191120_222636.jpg",
+    original: "../../assets/optimized/20191120_222636.jpg",
     thumbnail: "../../assets/thumbnail/20191120_222636.jpg",
   },
   {
-    original: "../../assets/original/20191120_222707.jpg",
+    original: "../../assets/optimized/20191120_222707.jpg",
     thumbnail: "../../assets/thumbnail/20191120_222707.jpg",
   },
   {
-    original: "../../assets/original/20191121_150905.jpg",
+    original: "../../assets/optimized/20191121_150905.jpg",
     thumbnail: "../../assets/thumbnail/20191121_150905.jpg",
   },
   {
-    original: "../../assets/original/20191121_151038.jpg",
+    original: "../../assets/optimized/20191121_151038.jpg",
     thumbnail: "../../assets/thumbnail/20191121_151038.jpg",
   },
   {
-    original: "../../assets/original/20191121_151243.jpg",
+    original: "../../assets/optimized/20191121_151243.jpg",
     thumbnail: "../../assets/thumbnail/20191121_151243.jpg",
   },
   {
-    original: "../../assets/original/20191121_151337.jpg",
+    original: "../../assets/optimized/20191121_151337.jpg",
     thumbnail: "../../assets/thumbnail/20191121_151337.jpg",
   },
   {
-    original: "../../assets/original/20191122_214436.jpg",
+    original: "../../assets/optimized/20191122_214436.jpg",
     thumbnail: "../../assets/thumbnail/20191122_214436.jpg",
   },
   {
-    original: "../../assets/original/20191125_212213.jpg",
+    original: "../../assets/optimized/20191125_212213.jpg",
     thumbnail: "../../assets/thumbnail/20191125_212213.jpg",
   },
   {
-    original: "../../assets/original/20191130_210953.jpg",
+    original: "../../assets/optimized/20191130_210953.jpg",
     thumbnail: "../../assets/thumbnail/20191130_210953.jpg",
   },
   {
-    original: "../../assets/original/20191201_211812.jpg",
+    original: "../../assets/optimized/20191201_211812.jpg",
     thumbnail: "../../assets/thumbnail/20191201_211812.jpg",
   },
   {
-    original: "../../assets/original/20191201_220606.jpg",
+    original: "../../assets/optimized/20191201_220606.jpg",
     thumbnail: "../../assets/thumbnail/20191201_220606.jpg",
   },
   {
-    original: "../../assets/original/20191203_191226.jpg",
+    original: "../../assets/optimized/20191203_191226.jpg",
     thumbnail: "../../assets/thumbnail/20191203_191226.jpg",
   },
   {
-    original: "../../assets/original/20191203_191304.jpg",
+    original: "../../assets/optimized/20191203_191304.jpg",
     thumbnail: "../../assets/thumbnail/20191203_191304.jpg",
   },
   {
-    original: "../../assets/original/20191120_000000.jpeg",
+    original: "../../assets/optimized/20191120_000000.jpg",
     thumbnail: "../../assets/thumbnail/20191120_000000.jpeg",
   },
 ];
